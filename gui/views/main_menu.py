d81d84d87772b896df0f45a44e427e33a8b48ee6
robert-e-frasier <robert.e.frasier@gmail.com>
2025-05-05 18:23:07 -0700

    Pushing updated code

--- gui/views/main_menu.py
+++ gui/views/main_menu.py
@@ -1,6 +1,7 @@
 from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
 
-
+Builder.load_file("gui/kv/main_menu.kv")
 
 class MainMenu(Screen):
     pass
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from kivy.app import App
 from gui.screen_manager import AppScreenManager
+from kivy.app import App
 from kivy.core.window import Window
-
-Window.clearcolor = (14/255, 47/255, 10/255, 1) 
 
 class FinanceApp(App):
     def build(self):
+        Window.clearcolor = (14/255, 47/255, 10/255, 1) # #0E2F0A
         return AppScreenManager()
     
 if __name__ == "__main__":
